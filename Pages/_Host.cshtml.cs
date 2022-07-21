37a8294df3ac9bbfb1b250d3874ecf9896cbd76a
Ivanov,Tihomir T.I <tihomir.ivanov@student.fontys.nl>
2022-07-21 19:21:19 +0300

    first commit

---
+++ Pages/_Host.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace All_Art.Pages
+{
+    public class _HostModel:PageModel
+    {
+        public void OnGet()
+        {
+            Response.Headers.Add("Cross-Origin-Resource-Policy", "cross-origin");
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+            Response.Headers.Add("Access-Control-Allow-Methods", "GET");
+            Response.Headers.Add("Access-Control-Allow-Credentials","true");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using All_Art.Data;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Cors;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddSession(options => { options.IdleTimeout = TimeSpan.FromSeconds(10); });
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(x => x.MapControllers()); 
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.Run();
